2700c0d3f7571aacca744fe49a16a80ea472db08
José Rafael <jrafaeldesantana@gmail.com>
2017-07-17 00:51:29 -0300

    Criação dos modelos

---
+++ Microblog/Microblog/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Microblog.Models
+{
+    public class Post
+    {
+        public int id { get; set; }
+        public int idUser { get; set; }
+        public string title { get; set; }
+        public DateTime date { get; set; }
+        public string content { get; set; }
+        public string urlImgFeatured { get; set; }
+        public bool isPublished { get; set; }
+
+    }
+}
---
+++ Microblog/Microblog/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Microblog.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string user { get; set; }
+        public string email { get; set; }
+        public string pass { get; set; }
+        public string name { get; set; }
+        public int rule { get; set; }
+    }
+}
